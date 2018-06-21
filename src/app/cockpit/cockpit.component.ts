bd0764f83f0c8c50bff04b52982967d696d75773
wes lawler <wes.lawler2@gmail.com>
2018-06-20 19:16:34 -0500

    completed section 5 lecture 64 but need to match example

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,28 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
+ @Output() serverCreated = new EventEmitter<{serverName:string, serverContent:string }>();
+ @Output() blueprintCreated = new EventEmitter<{serverName:string, serverContent:string }>();
   newServerName = '';
   newServerContent = '';
 
   constructor() { }
 
   ngOnInit() {
-
-  onAddServer() {
-   // this.serverElements.push({
-    //  type: 'server',
-   //   name: this.newServerName,
-   //   content: this.newServerContent
-  //  });
+ 
+  }
+  onAddServer(serverData: {serverName:string, serverContent:string }) {
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+    serverContent:this.newServerContent});
+      
+  
   }
 
-  onAddBlueprint() {
- //   this.serverElements.push({
- //     type: 'blueprint',
- //     name: this.newServerName,
- //     content: this.newServerContent
- //   });
-  }
-  }
+ onAddBlueprint(blueprintData: {serverName:string, serverContent:string }) {
+  this.blueprintCreated.emit({
+    serverName: this.newServerName,
+  serverContent:this.newServerContent});
+     
 
-}
+  }}
