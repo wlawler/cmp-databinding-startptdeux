bc770ea50b0e9520fb8b0afc2b16dcacb0a8fcda
wes lawler <wes.lawler2@gmail.com>
2018-07-05 10:31:17 -0500

    finished section 5 lecture 75

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -12,7 +12,8 @@ import {
   AfterViewChecked,
   OnDestroy,
   ViewChild,
-ElementRef}
+ElementRef,
+ContentChild}
        from '@angular/core';
 
 @Component({
@@ -34,6 +35,7 @@ export class ServerElementComponent implements
 @Input('srvElement') element: {type: string, name: string, content: string};
 @Input() name:string;
 @ViewChild('heading') header: ElementRef;
+@ContentChild('contentParagraph') paragraph: ElementRef;
   constructor() {
     console.log('constructor called!');
    }
@@ -45,6 +47,7 @@ ngOnChanges(changes: SimpleChanges) {
   ngOnInit() {
     console.log('ngOnInIt called!')
     console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text  Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
   ngDoCheck(){
@@ -54,6 +57,7 @@ ngOnChanges(changes: SimpleChanges) {
 
   ngAfterContentInit(){
     console.log('ngAfterContentInit called!');
+    console.log('Text  Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 ngAfterContentChecked(){
   console.log('ngAfterContentChecked called!');
